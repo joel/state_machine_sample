a2b950444355dfe350d9a12aee4ddbfd45831408
Joel AZEMAR <joel.azemar@gmail.com>
2012-07-04 18:28:23 +0200

    Try another configuration for memcached dalli

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,14 +7,4 @@ StateMachineSample::Application.config.session_store :cookie_store, key: '_state
 # (create the session table with "rails generate session_migration")
 # StateMachineSample::Application.config.session_store :active_record_store
 
-# Session cache
-ActionController::Base.session = {
-  :namespace   => 'sessions',
-  :expire_after => 20.minutes.to_i,
-  :memcache_server => ['mc2.ec2.memcachier.com:11211'],
-  :key         => 'c43f9b',
-  :secret      => 'cef01b06baafe5c1eb16'
-}
-
-require 'action_controller/session/dalli_store'
-ActionController::Base.session_store = :dalli_store
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes if Rails.env.production?
